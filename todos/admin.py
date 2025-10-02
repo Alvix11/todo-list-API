35e5b5ad30318662f813885267efab35664de7cd
Alvix <alvix.wick@gmail.com>
2025-10-02 10:49:57 -0400

    Add User model and customize admin panel

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from todos.models import User
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ['email', 'username', 'is_staff', 'is_active']
+    search_fields = ['email', 'username']
+    
+admin.site.register(User, CustomUserAdmin)
