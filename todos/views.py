9d348f3a1e29c47f7b00b1ef0e0fb21dcaec98bd
Alvix <alvix.wick@gmail.com>
2025-10-02 18:57:49 -0400

    Create login view

--- todos/views.py
+++ todos/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import UserRegisterSerializer
+from .serializers import UserRegisterSerializer, UserLoginSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
@@ -19,7 +19,26 @@ class UserRegisterView(APIView):
         
         if serializer.is_valid():
             user = serializer.save()
-            token = RefreshToken.for_user(user)
-            return Response(str(token.access_token), status=status.HTTP_201_CREATED)
+            refresh = RefreshToken.for_user(user)
+            return Response(str(refresh.access_token), status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class UserLoginView(APIView):
+    authentication_classes = []
+    permission_classes = []
+    
+    def post(self, request):
+        
+        serializer = UserLoginSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            
+            user = serializer.validated_data['user']
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                            'token': str(refresh.access_token)
+                            }, status=status.HTTP_200_OK
+                            )
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
