0c1d5fd0da32ee51344554ff797ee5ba5a42e057
Alvix <alvix.wick@gmail.com>
2025-10-02 18:02:23 -0400

    Finish user registration view and add access token

--- todos/views.py
+++ todos/views.py
@@ -2,19 +2,24 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import UserRegistrationSerializer
+from .serializers import UserRegisterSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
-class UserRegistrationView(APIView):
+class UserRegisterView(APIView):
+    
+    authentication_classes = []
+    permission_classes = []
     
     def post(self, request):
         """
         Handles POST request for user registration
         """
-        serializer = UserRegistrationSerializer(data=request.data)
+        serializer = UserRegisterSerializer(data=request.data)
         
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            user = serializer.save()
+            token = RefreshToken.for_user(user)
+            return Response(str(token.access_token), status=status.HTTP_201_CREATED)
         
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
