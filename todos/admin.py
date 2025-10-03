255e2bf393738125ccaa5e12370f9cccce4f41b9
Alvix <alvix.wick@gmail.com>
2025-10-02 20:43:41 -0400

    Register Task model in admin

--- todos/admin.py
+++ todos/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from todos.models import User
+from todos.models import User, Task
 
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
     list_display = ['email', 'username', 'is_staff', 'is_active']
     search_fields = ['email', 'username']
-    
+
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(Task)
