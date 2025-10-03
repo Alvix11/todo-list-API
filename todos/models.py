90803d904cacaa017a59ad170f603b86e6d09514
Alvix <alvix.wick@gmail.com>
2025-10-02 20:46:29 -0400

    Add method to display task title in admin

--- todos/models.py
+++ todos/models.py
@@ -14,3 +14,6 @@ class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField()
+    
+    def __str__(self):
+        return self.title
