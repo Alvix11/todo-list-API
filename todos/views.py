eb0e486057327cd25515abe9aa67b9454438f12e
Alvix <alvix.wick@gmail.com>
2025-10-03 23:22:50 -0400

    Add task update view and docstrings

--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -6,6 +6,7 @@ from .serializers import UserRegisterSerializer, UserLoginSerializer, TaskSerial
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from .models import Task
 
 # Create your views here.
 class UserRegisterView(APIView):
@@ -56,6 +57,9 @@ class CreateTaskView(APIView):
     permission_classes = [IsAuthenticated]
     
     def post(self, request):
+        """
+        Handles POST for request create task
+        """
         
         data = request.data.copy()
         data['user'] = request.user.id
@@ -65,5 +69,28 @@ class CreateTaskView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UpdateTaskView(APIView):
+    
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def put(self, request, pk):
+        """
+        Handles POST for update task
+        """
+        
+        task = get_object_or_404(Task, pk=pk, user=request.user)
+        
+        if task is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = TaskSerializer(task, data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
