bb1297ce806e1ef4fdbfc4138ffcea78ec739080
Alvix <alvix.wick@gmail.com>
2025-10-05 09:45:59 -0400

    Modify Task model display in admin

--- todos/admin.py
+++ todos/admin.py
@@ -7,5 +7,8 @@ class CustomUserAdmin(UserAdmin):
     list_display = ['email', 'username', 'is_staff', 'is_active']
     search_fields = ['email', 'username']
 
+class CustomTask(admin.ModelAdmin):
+    list_display = ['title', 'user']
+
 admin.site.register(User, CustomUserAdmin)
-admin.site.register(Task)
+admin.site.register(Task, CustomTask)
