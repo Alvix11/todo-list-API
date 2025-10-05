3093132ac93f5bc19fdb5f0d92efb150db615669
Alvix <alvix.wick@gmail.com>
2025-10-05 19:37:30 -0400

    Add endpoint list task for user

--- todos/urls.py
+++ todos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserRegisterView, UserLoginView, TaskCreateView, TaskDetailView
+from .views import UserRegisterView, UserLoginView, TaskCreateView, TaskDetailView, TaskListView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('todos/', TaskCreateView.as_view(), name='create'),
     path('todos/<int:pk>/', TaskDetailView.as_view(), name='update'), 
+    path('todos', TaskListView.as_view(), name='list')
 ]
