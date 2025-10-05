b653fd3f8add46265dcb2ad98d5ca5bed4f72316
Alvix <alvix.wick@gmail.com>
2025-10-04 22:49:40 -0400

    Add endpoint update and delete task

--- todos/urls.py
+++ todos/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import UserRegisterView, UserLoginView, CreateTaskView, UpdateTaskView
+from .views import UserRegisterView, UserLoginView, TaskCreateView, TaskDetailView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
-    path('todos/', CreateTaskView.as_view(), name='create'),
-    path('todos/<int:pk>/', UpdateTaskView.as_view(), name='update')
+    path('todos/', TaskCreateView.as_view(), name='create'),
+    path('todos/<int:pk>/', TaskDetailView.as_view(), name='update'), 
 ]
