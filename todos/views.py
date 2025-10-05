a99d812c948987d30fcbd25be6f6405f5f6d25b7
Alvix <alvix.wick@gmail.com>
2025-10-05 10:06:52 -0400

    Modify token display in user registration

--- todos/views.py
+++ todos/views.py
@@ -24,7 +24,11 @@ class UserRegisterView(APIView):
         if serializer.is_valid():
             user = serializer.save()
             refresh = RefreshToken.for_user(user)
-            return Response(str(refresh.access_token), status=status.HTTP_201_CREATED)
+            return Response({
+                            'token': str(refresh.access_token)
+                            }, 
+                            status=status.HTTP_201_CREATED
+                            )
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
