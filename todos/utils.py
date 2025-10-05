03c457cce0e3213b9f3c862df5f6d6681c2fd2e9
Alvix <alvix.wick@gmail.com>
2025-10-04 23:55:25 -0400

    Create method to verify task ownership using existing task verification function

--- todos/utils.py
+++ todos/utils.py
@@ -1,10 +1,21 @@
 from .models import Task
 
-def get_task(pk, user):
+def get_task(pk):
     
     try:
-        task = Task.objects.get(pk=pk, user=user)
+        task = Task.objects.get(pk=pk)
         return task
     except Task.DoesNotExist:
-        task = None
+        return None
+    
+def get_task_user(pk, user):
+    
+    task = get_task(pk=pk)
+    
+    if task is None:
+        return 404
+    
+    if task.user == user:
         return task
+    else:
+        return 403
