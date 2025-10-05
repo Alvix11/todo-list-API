7bf35c70a784c2989c98143dc7b699dc45dcbbc8
Alvix <alvix.wick@gmail.com>
2025-10-04 21:42:51 -0400

    Return 403 Forbidden when no Bearer token is provided and remove unused code

--- core/exceptions.py
+++ core/exceptions.py
@@ -2,27 +2,19 @@ from rest_framework.views import exception_handler
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.exceptions import AuthenticationFailed, NotAuthenticated
-from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
 
 def custom_exception_handler(exc, context):
     """
     Handles exceptions globally so as not to display so much information
     """
     response = exception_handler(exc, context)
-
-    if response is None:
-        if isinstance(exc, (InvalidToken, TokenError)):
-            return Response(
-                {"message": "Unauthorized"},
-                status=status.HTTP_401_UNAUTHORIZED
-            )
-        return response
     
     if isinstance(exc, NotAuthenticated):
         return Response(
-            {"message": "Unauthorized"},
-            status=status.HTTP_401_UNAUTHORIZED
+            {"message": "Forbidden"},
+            status=status.HTTP_403_FORBIDDEN
             )
+        
     elif isinstance(exc, AuthenticationFailed):
         return Response(
             {"message": "Unauthorized"},
