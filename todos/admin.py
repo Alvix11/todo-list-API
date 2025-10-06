23ff6492d01bf0adab0d9728a7f2fb47b8e38f89
Alvix <alvix.wick@gmail.com>
2025-10-06 16:06:59 -0400

    Customize Task admin display

--- todos/admin.py
+++ todos/admin.py
@@ -4,12 +4,24 @@ from todos.models import User, Task
 
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
-    list_display = ['email', 'username', 'is_staff', 'is_active', 'id']
+    list_display = ['id', 'email', 'username', 'is_staff', 'is_active']
     search_fields = ['email', 'username']
 
 class CustomTask(admin.ModelAdmin):
-    list_display = ['title', 'user', 'id']
-    readonly_fields = ['id']
+    readonly_fields = [ 'id']
+    list_display = ['id', 'title', 'get_user_id', 'get_user_username']
+    list_filter = ['user']
+    
+    def get_user_id(self, obj):
+        return obj.user.id
+    
+    get_user_id.short_description = 'User ID'
+    get_user_id.admin_order_field = 'user__id'
+    
+    def get_user_username(self, obj):
+        return obj.user
+    
+    get_user_username.short_description = 'Username'
 
 admin.site.register(User, CustomUserAdmin)
 admin.site.register(Task, CustomTask)
