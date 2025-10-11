398a2e9b46a72791c18b1c99eaef1875d33f00c3
Alvix <alvix.wick@gmail.com>
2025-10-10 23:55:26 -0400

    refactor: Update URLs to work with restructured view classes

--- todos/urls.py
+++ todos/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import UserRegisterView, UserLoginView, TaskCreateView, TaskDetailView, TaskListView
+from .views import UserRegisterView, UserLoginView, TaskCreateView, TaskDetailView, TaskListView, TaskUpdateView, TaskDeleteView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
-    path('todos/', TaskCreateView.as_view(), name='create'),
-    path('todos/<int:pk>/', TaskDetailView.as_view(), name='detail'), 
-    path('todos', TaskListView.as_view(), name='list')
+    path('todos/create', TaskCreateView.as_view(), name='create'),
+    path('todos/<int:pk>/', TaskDetailView.as_view(), name='detail'),
+    path('todos/<int:pk>/update', TaskUpdateView.as_view(), name='update'),
+    path('todos/<int:pk>/delete', TaskDeleteView.as_view(), name='delete'),
+    path('todos/', TaskListView.as_view(), name='list'),
 ]
